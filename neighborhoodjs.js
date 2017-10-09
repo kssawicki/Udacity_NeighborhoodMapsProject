033a4e1c0fb6bacd13f36b7268de1880c2286291
kssawicki <sawicki.katherine@gmail.com>
2017-10-08 22:53:29 -0700

    Added more markers code, but still don't get it!

--- neighborhoodjs.js
+++ neighborhoodjs.js
@@ -1,79 +1,83 @@
+'use strict';
+
+// Information about the places these dogs live
+    var locations = [
+    {title: 'Ziggy', location: {lat: 39.5050151, lng: -119.812267}},
+    {title: 'Frank & Oreo', location: {lat: 39.419104, lng: -119.772268}},
+    {title: 'Valentina', location: {lat: 39.512546, lng: -119.813448}},
+    {title: 'Milo', location: {lat: 39.504568, lng: -119.809846}},
+    {title: 'Kimbo', location: {lat: 39.576616, lng: -119.824863}},
+
+];
+
 // Initializing map
 var map;
 function initMap() {
-	"use strict";
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 39.530895, lng: -119.814972},
 		zoom: 14
 	});
-}
 
-// Alert user if the map isn't working
-function googleError() {
-	"use strict";
-	document.getElementById('error').innerHTML = "<h2>Google Maps is not responding. Try refreshing the page later.</h2>";
-}
+// Markers and infowindows
+var infowindow = new google.maps.InfoWindow();
+
+// Create a new blank array for all the markers
+var markers = [];
+
+ // The following group uses the location array to create an array of markers on initialize.
+        for (var i = 0; i < locations.length; i++) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
+           var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open an infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+        }
+        document.getElementById('show-dogs').addEventListener('click', show-dogs);
+        document.getElementById('hide-dogs').addEventListener('click', hide-dogs);
+      }
+
+       // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+        }
+      }
+
+      // This function will loop through the markers array and display them all.
+      function showListings() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+      // This function will loop through the listings and hide them all.
+      function hideListings() {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      }
 
 
-// ViewModel
-var ViewModel = function () {
-    "use strict";
-
-    // Create an array of all places
-   
-    // Initalize infowindow
-    var infowindow = new google.maps.InfoWindow({
-    	maxWidth: 200,
-    });
-
-    //Initalize marker
-    var marker = new google.maps.Marker ({
-        position: dogs,
-        title: ''
-    });
-
-document.getElementById('show-dogs').addEventListener('click', show-dogs);
-document.getElementById('hide-dogs').addEventListener('click', hide-dogs);
-
-
-// Information about the places these dogs live
-    var dogs = [
-    {
-        name: 'Ziggy',
-	    lat: 39.5050151,
-	    lng: -119.812267,
-        id: 'ziggy'
-    },
-    {
-        name: 'Pogo',
-        lat: 39.534116,
-        lng: -119.877041,
-        id: 'pogo'
-    },
-    {
-        name: 'Frank & Oreo',
-        lat: 39.419104,
-        lng: -119.772268,
-        id: 'f_o'
-    },
-     {
-        name: 'Valentina',
-        lat: 39.512546,
-        lng: -119.813448,
-        id: 'valentina'
-    },
-    {
-        name: 'Milo',
-        lat: 39.504568,
-        lng: -119.809846,
-        id: 'milo'
-    },
-    {
-        name: 'Kimbo',
-        lat: 39.576616,
-        lng: -119.824863,
-        id: 'kimbo'
-    },
-    ];
-
-}
